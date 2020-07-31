895bd7502101f0d832affcd10f1278c3906a278f
Sangwa-C <chelseasabinesangwa@gmail.com>
2020-07-31 08:37:19 -0700

    edits

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
+// Route::get('students', 'ApiController@getAllStudents');
+// Route::get('students/{id}', 'ApiController@getStudent');
+// Route::post('students', 'ApiController@createStudent');
+// Route::put('students/{id}', 'ApiController@updateStudent');
+// Route::delete('students/{id}','ApiController@deleteStudent');
+
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
